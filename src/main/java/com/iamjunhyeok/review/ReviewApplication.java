4d6a7603cf1d901ed1ce343a5d720bf96e0e53b8
iamjunhyeok <jeonjhyeok@gmail.com>
2024-06-10 18:27:57 +0900

    EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/iamjunhyeok/review/ReviewApplication.java
+++ src/main/java/com/iamjunhyeok/review/ReviewApplication.java
@@ -2,7 +2,9 @@ package com.iamjunhyeok.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ReviewApplication {
 
