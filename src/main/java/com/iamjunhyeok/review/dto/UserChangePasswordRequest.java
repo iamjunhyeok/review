872497d931b859f0b6f5f7e8a2f617ef670450f1
iamjunhyeok <jeonjhyeok@gmail.com>
2024-06-11 18:24:46 +0900

    정적 팩토리 메서드 생성

--- src/main/java/com/iamjunhyeok/review/dto/UserChangePasswordRequest.java
+++ src/main/java/com/iamjunhyeok/review/dto/UserChangePasswordRequest.java
@@ -8,4 +8,11 @@ import lombok.Setter;
 public class UserChangePasswordRequest {
     private String oldPassword;
     private String newPassword;
+
+    public static UserChangePasswordRequest of(String oldPassword, String newPassword) {
+        UserChangePasswordRequest userChangePasswordRequest = new UserChangePasswordRequest();
+        userChangePasswordRequest.setOldPassword(oldPassword);
+        userChangePasswordRequest.setNewPassword(newPassword);
+        return userChangePasswordRequest;
+    }
 }
--- src/main/java/com/iamjunhyeok/review/dto/UserJoinRequest.java
+++ src/main/java/com/iamjunhyeok/review/dto/UserJoinRequest.java
@@ -8,4 +8,11 @@ import lombok.Setter;
 public class UserJoinRequest {
     private String email;
     private String password;
+
+    public static UserJoinRequest of(String email, String password) {
+        UserJoinRequest request = new UserJoinRequest();
+        request.setEmail(email);
+        request.setPassword(password);
+        return request;
+    }
 }
