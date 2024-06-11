e4127bb687f5d834dedd57c69bb11ab5fb1994a2
iamjunhyeok <jeonjhyeok@gmail.com>
2024-06-11 22:38:14 +0900

    정적 팩토리 메서드 생성

--- src/main/java/com/iamjunhyeok/review/dto/CampaignApplyRequest.java
+++ src/main/java/com/iamjunhyeok/review/dto/CampaignApplyRequest.java
@@ -8,4 +8,11 @@ import lombok.Setter;
 public class CampaignApplyRequest {
     private String name;
     private String phoneNumber;
+
+    public static CampaignApplyRequest of(String name, String phoneNumber) {
+        CampaignApplyRequest request = new CampaignApplyRequest();
+        request.setName(name);
+        request.setPhoneNumber(phoneNumber);
+        return request;
+    }
 }
