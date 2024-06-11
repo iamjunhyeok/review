d1a0fba4d7e7bba12d52fb2d565e0ebcdbf0d753
iamjunhyeok <jeonjhyeok@gmail.com>
2024-06-11 18:25:20 +0900

    비밀번호 오류 예외처리 추가

--- src/main/java/com/iamjunhyeok/review/exception/ErrorCode.java
+++ src/main/java/com/iamjunhyeok/review/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "중복된 이메일입니다."),
     CAMPAIGN_NOT_FOUND(HttpStatus.NOT_FOUND, "캠페인을 찾을 수 없습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    INCORRECT_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
