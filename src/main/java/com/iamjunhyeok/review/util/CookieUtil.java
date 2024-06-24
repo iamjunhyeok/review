98c97bf37330ebe8de7fe0481b8b370249663ca5
iamjunhyeok <jeonjhyeok@gmail.com>
2024-06-24 23:03:53 +0900

    Cookie Util 생성

---
+++ src/main/java/com/iamjunhyeok/review/util/CookieUtil.java
@@ -0,0 +1,25 @@
+package com.iamjunhyeok.review.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+    public static void createCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setMaxAge(maxAge);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+    }
+
+    public static void deleteCookie(HttpServletResponse response, String name) {
+        Cookie cookie = new Cookie(name, null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+    }
+}
