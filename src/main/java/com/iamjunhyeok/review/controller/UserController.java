bc791b5118898f7320ecac25c0b6d1888480f00c
iamjunhyeok <jeonjhyeok@gmail.com>
2024-06-27 00:39:43 +0900

    사용자 관련 미사용 API 삭제

--- src/main/java/com/iamjunhyeok/review/controller/UserController.java
+++ src/main/java/com/iamjunhyeok/review/controller/UserController.java
@@ -1,18 +1,13 @@
 package com.iamjunhyeok.review.controller;
 
-import com.iamjunhyeok.review.dto.UserJoinRequest;
-import com.iamjunhyeok.review.dto.UserJoinResponse;
 import com.iamjunhyeok.review.dto.UserUpdateInfoRequest;
 import com.iamjunhyeok.review.dto.UserUpdatePasswordRequest;
 import com.iamjunhyeok.review.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,11 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-
-    @PostMapping
-    public ResponseEntity<UserJoinResponse> join(@RequestBody @Valid UserJoinRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(UserJoinResponse.from(userService.join(request)));
-    }
 
     @PatchMapping("/me/update-password")
     public ResponseEntity<Void> updatePassword(@RequestBody @Valid UserUpdatePasswordRequest request) {
@@ -45,10 +35,5 @@ public class UserController {
     public ResponseEntity<Void> updateUserInfo(@PathVariable Long id, @RequestBody @Valid UserUpdateInfoRequest request) {
         userService.updateUserInfo(id, request);
         return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/nickname/generate")
-    public String generateNickname() {
-        return userService.generateNickname();
     }
 }
--- src/main/java/com/iamjunhyeok/review/service/UserService.java
+++ src/main/java/com/iamjunhyeok/review/service/UserService.java
@@ -1,26 +1,13 @@
 package com.iamjunhyeok.review.service;
 
-import com.iamjunhyeok.review.constant.VocaType;
-import com.iamjunhyeok.review.domain.User;
-import com.iamjunhyeok.review.domain.Voca;
-import com.iamjunhyeok.review.dto.UserJoinRequest;
 import com.iamjunhyeok.review.dto.UserUpdateInfoRequest;
 import com.iamjunhyeok.review.dto.UserUpdatePasswordRequest;
-import com.iamjunhyeok.review.exception.ApplicationException;
 import com.iamjunhyeok.review.exception.ErrorCode;
 import com.iamjunhyeok.review.repository.UserRepository;
-import com.iamjunhyeok.review.repository.VocaRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
-import static java.util.stream.Collectors.groupingBy;
 
 @Service
 @RequiredArgsConstructor
@@ -29,23 +16,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    private final VocaRepository vocaRepository;
-
     private final PasswordEncoder passwordEncoder;
-
-    @Transactional
-    public User join(UserJoinRequest request) {
-        if (userRepository.existsByEmail(request.getEmail())) {
-            throw ErrorCode.DUPLICATE_EMAIL.build();
-        }
-        if (userRepository.existsByNickname(request.getNickname())) {
-            throw ErrorCode.DUPLICATE_NICKNAME.build();
-        }
-        if (!request.getPassword().equals(request.getConfirmPassword())) {
-            throw ErrorCode.PASSWORDS_DO_NOT_MATCH.build();
-        }
-        return userRepository.save(User.createUser(request.getEmail(), request.getNickname(), passwordEncoder.encode(request.getPassword())));
-    }
 
     @Transactional
     public void updatePassword(UserUpdatePasswordRequest request) {
@@ -77,24 +48,5 @@ public class UserService {
                 .ifPresentOrElse(user -> user.updateInfo(request.getNickname(), request.getAddress(), request.getRest(), request.getPostalCode()), () -> {
                     throw ErrorCode.USER_NOT_FOUND.build();
                 });
-    }
-
-    public String generateNickname() {
-        Map<VocaType, List<Voca>> groupedByType = vocaRepository.findAll()
-                .stream().collect(groupingBy(Voca::getType));
-        List<Voca> adjectives = groupedByType.get(VocaType.ADJECTIVE);
-        List<Voca> nouns = groupedByType.get(VocaType.NOUN);
-        StringBuilder nickname;
-        int attempts = 0;
-        do {
-            if (attempts > 100) {
-                throw new ApplicationException(HttpStatus.INTERNAL_SERVER_ERROR, "에러가 발생했습니다.");
-            }
-            nickname = new StringBuilder();
-            nickname.append(adjectives.get(new Random().nextInt(adjectives.size())).getValue());
-            nickname.append(nouns.get(new Random().nextInt(adjectives.size())).getValue());
-            attempts++;
-        } while (userRepository.existsByNickname(nickname.toString()));
-        return nickname.toString();
     }
 }
