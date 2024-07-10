a37daee8e26574bf7871cc78bda4e9b19ea4efa1
iamjunhyeok <jeonjhyeok@gmail.com>
2024-07-10 21:10:42 +0900

    누락된 프로젝션 등록

--- src/main/java/com/iamjunhyeok/review/config/BlazeConfig.java
+++ src/main/java/com/iamjunhyeok/review/config/BlazeConfig.java
@@ -8,6 +8,7 @@ import com.blazebit.persistence.view.EntityViews;
 import com.blazebit.persistence.view.spi.EntityViewConfiguration;
 import com.iamjunhyeok.review.dto.CampaignImageProjection;
 import com.iamjunhyeok.review.dto.CampaignLinkProjection;
+import com.iamjunhyeok.review.dto.CampaignSummaryProjection;
 import com.iamjunhyeok.review.repository.CampaignViewProjection;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +33,7 @@ public class BlazeConfig {
         config.addEntityView(CampaignViewProjection.class);
         config.addEntityView(CampaignLinkProjection.class);
         config.addEntityView(CampaignImageProjection.class);
+        config.addEntityView(CampaignSummaryProjection.class);
         return config.createEntityViewManager(criteriaBuilderFactory);
     }
 }
