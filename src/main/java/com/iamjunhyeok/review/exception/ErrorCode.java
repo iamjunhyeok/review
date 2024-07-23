eb36ebacc0c1336f24c60e17fb6f7cb23209bfe6
iamjunhyeok <jeonjhyeok@gmail.com>
2024-07-23 12:14:01 +0900

    토큰 만료 에러코드 추가

--- src/main/java/com/iamjunhyeok/review/exception/ErrorCode.java
+++ src/main/java/com/iamjunhyeok/review/exception/ErrorCode.java
@@ -25,7 +25,8 @@ public enum ErrorCode {
     PENALTY_NOT_FOUND(HttpStatus.NOT_FOUND, "패널티를 찾을 수 없습니다."),
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "리뷰를 찾을 수 없습니다."),
     DATA_DICTIONARY_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "항목을 찾을 수 없습니다."),
-    APPLICATION_CANNOT_BE_DELETED(HttpStatus.BAD_REQUEST, "내역을 삭제할 수 없습니다.");
+    APPLICATION_CANNOT_BE_DELETED(HttpStatus.BAD_REQUEST, "내역을 삭제할 수 없습니다."),
+    EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "로그인 정보가 만료되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
