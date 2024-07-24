dc50e771584ddde88ac4cb73c050fdc27348a318
iamjunhyeok <jeonjhyeok@gmail.com>
2024-07-24 11:00:29 +0900

    주석 추가

--- src/main/java/com/iamjunhyeok/review/controller/RefreshTokenController.java
+++ src/main/java/com/iamjunhyeok/review/controller/RefreshTokenController.java
@@ -15,6 +15,11 @@ public class RefreshTokenController {
 
     private final RefreshTokenService refreshTokenService;
 
+    /**
+     * 리프레시토큰을 이용하여 새 엑세스토큰 발급
+     * @param request
+     * @param response
+     */
     @PostMapping("/token/refresh")
     @ResponseStatus(HttpStatus.CREATED)
     public void refresh(HttpServletRequest request, HttpServletResponse response) {
