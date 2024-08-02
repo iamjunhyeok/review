e3042091ee46a9ac0f687715f1cbcf820d039e30
iamjunhyeok <jeonjhyeok@gmail.com>
2024-08-03 04:32:51 +0900

    Category 필드 추가

--- src/main/java/com/iamjunhyeok/review/projection/InquiryProjection.java
+++ src/main/java/com/iamjunhyeok/review/projection/InquiryProjection.java
@@ -1,6 +1,7 @@
 package com.iamjunhyeok.review.projection;
 
 import com.blazebit.persistence.view.EntityView;
+import com.iamjunhyeok.review.constant.InquiryCategory;
 import com.iamjunhyeok.review.domain.Inquiry;
 
 import java.time.LocalDateTime;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 @EntityView(Inquiry.class)
 public interface InquiryProjection {
     Long getId();
+    InquiryCategory getCategory();
     String getTitle();
     String getContent();
     LocalDateTime getCreatedAt();
