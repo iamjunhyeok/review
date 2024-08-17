f557be5001eb7ba054d0e84ab5c1f5f6ab417941
iamjunhyeok <jeonjhyeok@gmail.com>
2024-08-17 20:39:49 +0900

    내 캠페인 조회 시, 신청정보 필드, 수정요청 리뷰 수 필드 추가

--- src/main/java/com/iamjunhyeok/review/projection/ApplicationSearchProjection.java
+++ src/main/java/com/iamjunhyeok/review/projection/ApplicationSearchProjection.java
@@ -1,20 +1,46 @@
 package com.iamjunhyeok.review.projection;
 
+import com.blazebit.persistence.SubqueryInitiator;
 import com.blazebit.persistence.view.EntityView;
 import com.blazebit.persistence.view.IdMapping;
+import com.blazebit.persistence.view.MappingSubquery;
+import com.blazebit.persistence.view.SubqueryProvider;
 import com.iamjunhyeok.review.constant.ApplicationStatus;
+import com.iamjunhyeok.review.constant.ReviewStatus;
 import com.iamjunhyeok.review.domain.Application;
-
-import java.util.List;
+import com.iamjunhyeok.review.domain.Review;
 
 @EntityView(Application.class)
 public interface ApplicationSearchProjection {
     @IdMapping
     Long getId();
 
+    String getName();
+
+    String getPhoneNumber();
+
+    String getAddress();
+
+    String getRest();
+
+    String getPostalCode();
+
     ApplicationStatus getStatus();
 
     CampaignSummaryProjection getCampaign();
 
-    List<ReviewStatusProjection> getReviews();
+    @MappingSubquery(ModifyRequestReviewCountSubqueryProvider.class)
+    Long getModifyRequestReviewCount();
+
+    class ModifyRequestReviewCountSubqueryProvider implements SubqueryProvider {
+
+        @Override
+        public <T> T createSubquery(SubqueryInitiator<T> subqueryInitiator) {
+            return subqueryInitiator.from(Review.class, "r")
+                    .select("COUNT(*)")
+                    .where("r.application.id").eqExpression("EMBEDDING_VIEW(id)")
+                    .where("r.status").eq(ReviewStatus.MODIFY_REQUEST)
+                    .end();
+        }
+    }
 }
