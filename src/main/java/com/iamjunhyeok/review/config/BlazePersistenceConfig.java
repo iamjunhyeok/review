9b000529f3eb0ff9d64494080c3c7216a5cc45b9
iamjunhyeok <jeonjhyeok@gmail.com>
2024-08-23 17:14:46 +0900

    @EnableJpaRepositories 주석처리

--- src/main/java/com/iamjunhyeok/review/config/BlazePersistenceConfig.java
+++ src/main/java/com/iamjunhyeok/review/config/BlazePersistenceConfig.java
@@ -4,7 +4,6 @@ import com.blazebit.persistence.Criteria;
 import com.blazebit.persistence.CriteriaBuilderFactory;
 import com.blazebit.persistence.integration.view.spring.EnableEntityViews;
 import com.blazebit.persistence.spi.CriteriaBuilderConfiguration;
-import com.blazebit.persistence.spring.data.impl.repository.BlazePersistenceRepositoryFactoryBean;
 import com.blazebit.persistence.view.EntityViewManager;
 import com.blazebit.persistence.view.spi.EntityViewConfiguration;
 import jakarta.persistence.EntityManagerFactory;
@@ -14,11 +13,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableEntityViews(basePackages = "com.iamjunhyeok.review.projection")
-@EnableJpaRepositories(basePackages = "com.iamjunhyeok.review.repository",
-        repositoryFactoryBeanClass = BlazePersistenceRepositoryFactoryBean.class)
+//@EnableJpaRepositories(basePackages = "com.iamjunhyeok.review.repository",
+//        repositoryFactoryBeanClass = BlazePersistenceRepositoryFactoryBean.class)
 @Configuration
 public class BlazePersistenceConfig {
 
