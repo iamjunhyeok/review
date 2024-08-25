dfe06ab35f23be510d5fb8b338a624a22fed7a41
iamjunhyeok <jeonjhyeok@gmail.com>
2024-08-25 14:40:33 +0900

    user_id 필드 not null

--- src/main/java/com/iamjunhyeok/review/domain/Campaign.java
+++ src/main/java/com/iamjunhyeok/review/domain/Campaign.java
@@ -71,7 +71,7 @@ public class Campaign extends CampaignBase {
     private List<CampaignOption> options = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public Campaign apply(Application application) {
