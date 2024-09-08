83d26a5791bef7f06a565501ca37e0ce8155c11e
iamjunhyeok <jeonjhyeok@gmail.com>
2024-09-08 12:50:49 +0900

    PreAuthorize 어노테이션 클래스에 적용

--- src/main/java/com/iamjunhyeok/review/controller/AdminController.java
+++ src/main/java/com/iamjunhyeok/review/controller/AdminController.java
@@ -26,6 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@PreAuthorize("hasRole('ADMIN')")
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
@@ -33,7 +34,6 @@ public class AdminController {
 
     private final AdminService adminService;
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/banners/{codeId}")
     @ResponseStatus(HttpStatus.CREATED)
     public void registerBanner(@PathVariable Long codeId,
@@ -42,26 +42,22 @@ public class AdminController {
         adminService.registerBanner(codeId, request, file);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/banners")
     public ResponseEntity<List<BannerProjection>> fetchAll() {
         return ResponseEntity.ok(adminService.fetchAll());
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/plans")
     @ResponseStatus(HttpStatus.CREATED)
     public void registerPlan(@RequestBody PlanRegisterRequest request) {
         adminService.registerPlan(request);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/plans")
     public ResponseEntity<List<PlanProjection>> fetchAllPlans() {
         return ResponseEntity.ok(adminService.fetchAllPlans());
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/campaigns")
     public ResponseEntity<List<CampaignSearchProjection>> fetchAllCampaigns(@RequestParam(required = false) String type,
                                                                  @RequestParam(required = false) String categories,
@@ -73,14 +69,12 @@ public class AdminController {
         return ResponseEntity.ok(adminService.fetchAllCampaigns(type, categories, socials, options, status, pageable));
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/users/{userId}/plans/{planId}/subscribe")
     @ResponseStatus(HttpStatus.CREATED)
     public void subscribe(@PathVariable Long userId, @PathVariable Long planId) {
         adminService.subscribe(userId, planId);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/advertisers")
     public List<AdvertiserProjection> fetchAllAdvertisers(@RequestParam(required = false) String status) {
         return adminService.fetchAllAdvertisers(status);
